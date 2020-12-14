7bc01ed775180890864046d86774d49efb98f416
Lewis Njagi <lewismaybach@gmail.com>
2020-12-14 23:45:51 +0300

    add control flows and logical operators

--- js/index.js
+++ js/index.js
@@ -13,3 +13,21 @@ function akan() {
     var century= document.getElementById("century").value;
 
     var day = parseInt(((century / 4) - 2 * century - 1) + ((5 * years / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
+
+    if (century <19 || century > 20) {
+        alert("enter valid century");
+    }
+    if (mm <= 0 || mm > 12) {
+        alert("enter valid month");
+    } 
+    if (dd < 0 || dd > 31) {
+        alert("enter valid date");
+    }
+    if (years <00 || years =="") {
+        alert("invalid year");
+
+    } else if (gender === "male") {
+        alert("Your Akan male name is" + maleName[day]);
+    } else if (gender === "female") {
+        alert("Your Akan female name is" + femaleName[day]);
+    }
